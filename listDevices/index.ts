c59a5203daadbde1a1593018f43f2795d05d80de
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-01-24 14:04:44 +0100

    fix: deploying from scratch now works

--- listDevices/index.ts
+++ listDevices/index.ts
@@ -1,9 +1,9 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 
-const httpTrigger: AzureFunction = async function(
+const listDevices: AzureFunction = async (
 	context: Context,
 	req: HttpRequest,
-): Promise<void> {
+): Promise<void> => {
 	context.log('HTTP trigger function processed a request.')
 	const name = req.query.name || (req.body && req.body.name)
 
@@ -20,4 +20,4 @@ const httpTrigger: AzureFunction = async function(
 	}
 }
 
-export default httpTrigger
+export default listDevices
