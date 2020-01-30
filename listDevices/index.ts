e0b0e6d2e2e21ca4ec68da8fdbe6e7aa4f8c0b38
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-01-30 16:58:50 +0100

    feat(function): use env var in function

--- listDevices/index.ts
+++ listDevices/index.ts
@@ -1,6 +1,16 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
-import { IotHubClient } from '@azure/arm-iothub'
-import * as msRestNodeAuth from '@azure/ms-rest-nodeauth'
+import { Registry } from 'azure-iothub'
+
+const connectionString = process.env.IOT_HUB_CONNECTION_STRING || ''
+
+const r = (result: any, status = 200) => ({
+	headers: {
+		'Content-Type': 'application/json; charset=uft-8',
+	},
+	status,
+	isRaw: true,
+	body: JSON.stringify(result),
+})
 
 const listDevices: AzureFunction = async (
 	context: Context,
@@ -8,33 +18,12 @@ const listDevices: AzureFunction = async (
 ): Promise<void> => {
 	context.log({ req })
 	try {
-		// See https://docs.microsoft.com/en-us/azure/app-service/overview-managed-identity?tabs=dotnet#obtaining-tokens-for-azure-resources
-		const msiTokenRes = await msRestNodeAuth.loginWithAppServiceMSI({
-			msiEndpoint: process.env.MSI_ENDPOINT || '',
-			msiSecret: process.env.MSI_SECRET || '',
-		})
-		console.log(msiTokenRes)
-
-		const iotHubClient = new IotHubClient(
-			msiTokenRes,
-			process.env.AZURE_SUBSCRIPTION_ID || '',
-		)
-		const iotHubs = await iotHubClient.iotHubResource.listBySubscription()
-
-		context.res = {
-			// status: 200, /* Defaults to 200 */
-			headers: {
-				'Content-Type': 'application/json; charset=uft-8',
-			},
-			isRaw: true,
-			body: JSON.stringify(iotHubs),
-		}
+		const registry = Registry.fromConnectionString(connectionString)
+		const devices = registry.createQuery('SELECT * FROM devices')
+		const res = await devices.nextAsTwin()
+		context.res = r(res.result)
 	} catch (error) {
-		context.res = {
-			status: 500,
-			isRaw: true,
-			body: JSON.stringify(error),
-		}
+		context.res = r(error, 500)
 	}
 }
 
