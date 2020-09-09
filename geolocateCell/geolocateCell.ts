7283fe6e073dbead7106ee103fca52e535f8d3d2
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-09-09 17:17:20 +0200

    feat(cellgeo): implement resolution

---
+++ geolocateCell/geolocateCell.ts
@@ -0,0 +1,66 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import { r } from '../lib/http'
+import { log } from '../lib/log'
+import { fromEnv } from '../lib/fromEnv'
+import { parseConnectionString } from '../lib/parseConnectionString'
+import { CosmosClient } from '@azure/cosmos'
+import {
+	cellId,
+	cellFromGeolocations,
+} from '@bifravst/cell-geolocation-helpers'
+import { isSome } from 'fp-ts/lib/Option'
+
+const { connectionString } = fromEnv({
+	connectionString: 'HISTORICAL_DATA_COSMOSDB_CONNECTION_STRING',
+})(process.env)
+
+const { AccountEndpoint, AccountKey } = parseConnectionString(connectionString)
+const cosmosClient = new CosmosClient({
+	endpoint: AccountEndpoint,
+	key: AccountKey,
+})
+
+const container = cosmosClient
+	.database('cellGeolocation')
+	.container('deviceCellGeolocations')
+
+const fromDeviceLocations = cellFromGeolocations({
+	minCellDiameterInMeters: 5000,
+	percentile: 0.9,
+})
+
+const getDevice: AzureFunction = async (
+	context: Context,
+	req: HttpRequest,
+): Promise<void> => {
+	log(context)({ req })
+
+	const { cell, area, mccmnc } = req.query
+	const c = cellId({
+		cell: parseInt(cell, 10),
+		area: parseInt(area, 10),
+		mccmnc: parseInt(mccmnc, 10),
+	})
+
+	try {
+		const sql = `SELECT c.lat AS lat, c.lng AS lng FROM c WHERE c.cellId='${c}'`
+		log(context)({ sql })
+		const locations = (await container.items.query(sql).fetchAll()).resources
+
+		log(context)({ locations })
+
+		const location = fromDeviceLocations(locations)
+
+		if (isSome(location)) {
+			context.res = r(location.value)
+			log(context)({ location: location.value })
+		} else {
+			context.res = r(new Error(`Could not resolve cell ${c}`), 404)
+		}
+	} catch (error) {
+		console.error({ error })
+		context.res = r(error, 500)
+	}
+}
+
+export default getDevice
